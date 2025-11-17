47a326658cea411e2ee5b3c116a2280d349935df
kannar <adamkannar2008@gmail.com>
2025-11-17 13:02:09 +0100

    elso

--- P20251117_1/Program.cs
+++ P20251117_1/Program.cs
@@ -10,6 +10,26 @@ namespace P20251117_1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Add meg az a értékét: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add meg a b értékét: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add meg a c értékét: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                if (a == 0)
+                {
+                    Console.WriteLine("Ez nem másodfokú egyenlet, mert a = 0.");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
